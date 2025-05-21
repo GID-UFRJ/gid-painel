87ae343a462d38880da4ed4af93a02575a62a6c3
gavieira <gabrieldeusdeth@gmail.com>
2025-05-21 13:11:05 -0300

    modelos registrados no app 'programa'

--- programa/admin.py
+++ programa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.AreaAvaliacao)
+admin.site.register(models.AreaBasica)
+admin.site.register(models.Centro)
+admin.site.register(models.Nivel)
+admin.site.register(models.Rede)
+admin.site.register(models.Situacao)
+admin.site.register(models.Unidade)
+admin.site.register(models.Programas)
