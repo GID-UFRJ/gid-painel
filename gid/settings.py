1bb134b8a6a830eeff713fe21f14ff2f543996e4
gavieira <gabrieldeusdeth@gmail.com>
2025-06-14 00:02:13 -0300

    adicionei chave default para o django

--- gid/settings.py
+++ gid/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='chave_super_secreta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', cast=bool, default=False)
