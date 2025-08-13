891e5e49ea4b0acbd5430f486823e0c418797744
gavieira <gabrieldeusdeth@gmail.com>
2025-08-13 12:44:31 -0300

    app sucupira apresenta página para cada ppg

--- sucupira/urls.py
+++ sucupira/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posgrad_ufrj/', views.posgrad_ufrj, name='posgrad_ufrj'),
     path('ppgs/', views.ppgs, name='ppgs'),
+    path('ppgs/<int:programa_id>/', views.ppg_detalhe, name='ppg_detalhe'),
 ]
--- sucupira/views.py
+++ sucupira/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse
+from .models import Programa
 
 # Create your views here.
 
@@ -9,5 +10,36 @@ def index(request):
 def posgrad_ufrj(request):
     return HttpResponse('Página em construção')
 
+
+
 def ppgs(request):
-    return HttpResponse('Página em construção')
+    """
+    Lista todos os programas de pós-graduação em ordem alfabética.
+    """
+    # Busca todos os objetos Programa e ordena pelo nome
+    programas = Programa.objects.all().order_by('nm_programa_ies')
+    
+    context = {
+        'programas': programas,
+    }
+    
+    return render(request, 'sucupira/ppgs.html', context)
+
+def ppg_detalhe(request, programa_id):
+    """
+    Exibe os detalhes de um programa de pós-graduação.
+    """
+    # Tenta buscar o objeto Programa pelo ID, ou retorna 404 se não existir.
+    programa = get_object_or_404(Programa, pk=programa_id)
+    
+    # Aqui, você pode buscar outros dados relacionados ao programa
+    # para exibir no gráfico, por exemplo, docentes ou discentes.
+    # Exemplo:
+    # docentes = programa.docente_set.all().order_by('ano__ano_valor')
+    
+    context = {
+        'programa': programa,
+        # 'docentes': docentes, # Adicione outros dados aqui
+    }
+    
+    return render(request, 'sucupira/ppg_detalhe.html', context)
