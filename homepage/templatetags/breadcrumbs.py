344536c6530b3a94bf08dbe622e32f220684b6d4
gavieira <gabrieldeusdeth@gmail.com>
2025-08-20 10:41:19 -0300

    Adicionado menu de navegação breadcrumb dinâmico

---
+++ homepage/templatetags/breadcrumbs.py
@@ -0,0 +1,24 @@
+from django import template
+from urllib.parse import unquote
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def breadcrumbs(context):
+    request = context['request']
+    path = request.path
+
+    print(f"DEBUG: request.path = {path}")  # <---- DEBUG
+
+    parts = [unquote(p) for p in path.strip('/').split('/') if p]
+    print(f"DEBUG: parts = {parts}")  # <---- DEBUG
+
+    breadcrumbs = []
+    url = '/'
+
+    for part in parts:
+        url += part + '/'
+        name = part.replace('-', ' ').replace('_', ' ').capitalize()
+        breadcrumbs.append((name, url))
+
+    return breadcrumbs
