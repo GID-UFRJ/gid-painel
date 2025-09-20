9ba8fe20bad0fd9cf0896643fa2fe00fb497dcde
gavieira <gabrieldeusdeth@gmail.com>
2025-09-20 17:24:35 -0300

    Commit anterior a criar partial para abas

--- sucupira/views.py
+++ sucupira/views.py
@@ -135,50 +135,6 @@ def ppgs(request):
     return render(request, 'sucupira/posgrad/ppgs.html', context)
 
 
-#def ppgs(request):
-#    """
-#    Lista todos os programas de pós-graduação por grande area em ordem alfabética.
-#    """
-#    # Buscar todos os programas com a grande_area já carregada
-#    programas = Programa.objects.select_related('grande_area').order_by(
-#        'grande_area__nm_grande_area_conhecimento', 'nm_programa_ies'
-#    )
-#
-#    # Agrupar por Grande Área
-#    agrupados = defaultdict(list)
-#    for programa in programas:
-#        chave = programa.grande_area.nm_grande_area_conhecimento
-#        agrupados[chave].append(programa)
-#
-#    # Transformar em dict ordenado por chave (Grande Área)
-#    programas_agrupados = dict(sorted(agrupados.items()))
-#
-#    context = {
-#        'programas_agrupados': programas_agrupados
-#    }
-#    return render(request, 'sucupira/posgrad/ppgs.html', context)
-
-
-#def ppg_detalhe(request, programa_id):
-#    """
-#    Exibe os detalhes de um programa de pós-graduação.
-#    """
-#    # Tenta buscar o objeto Programa pelo ID, ou retorna 404 se não existir.
-#    programa = get_object_or_404(Programa, pk=programa_id)
-#    
-#    # Aqui, você pode buscar outros dados relacionados ao programa
-#    # para exibir no gráfico, por exemplo, docentes ou discentes.
-#    # Exemplo:
-#    # docentes = programa.docente_set.all().order_by('ano__ano_valor')
-#    
-#    context = {
-#        'programa': programa,
-#        # 'docentes': docentes, # Adicione outros dados aqui
-#    }
-#    
-#    return render(request, 'sucupira/posgrad/ppg_detalhe.html', context)
-
-
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Avg
 import pandas as pd
@@ -276,37 +232,3 @@ def ppg_detalhe(request, programa_id):
         "cursos": cursos,
     }
     return render(request, "sucupira/posgrad/ppg_detalhe.html", context)
-
-
-
-#def ppg_detalhe(request, programa_id):
-#    programa = get_object_or_404(Programa, pk=programa_id)
-#
-#    cursos = (
-#        Curso.objects
-#        .filter(programa=programa)
-#        .select_related("grau_curso")
-#        .order_by("grau_curso__nm_grau_curso")
-#    )
-#
-#    # Ano mais recente deste programa
-#    ano_programa = (
-#        programa.ano_programa
-#        .select_related("grande_area", "area_avaliacao", "situacao")
-#        .order_by("-ano__ano_valor")
-#        .first()
-#    )
-#
-#    graficos = {}
-#    for grau_nome in ["Mestrado", "Doutorado", "Mestrado Acadêmico"]:
-#        grafico_html = gerar_grafico(programa_id, grau_nome)
-#        if grafico_html:
-#            graficos[grau_nome] = grafico_html
-#
-#    context = {
-#        "programa": programa,
-#        "ano_programa": ano_programa,
-#        "graficos": graficos,
-#        "cursos": cursos,
-#    }
-#    return render(request, "sucupira/posgrad/ppg_detalhe.html", context)
